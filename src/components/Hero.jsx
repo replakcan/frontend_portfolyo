f2d4e54e1fec34b8d54ba0d4c8d1080398f8462b
replakcan <alper.makcan@gmail.com>
2024-10-05 17:22:34 +0300

    Updates hero css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,23 @@
 const Hero = () => {
-  return <section className="hero-section">Hero</section>;
+  return (
+    <section className="hero-section">
+      <div className="hero-content">
+      <div className="header-container">alper</div>
+        <div className="hero-left">
+          <h1>I am a Frontend Developer...</h1>
+          <p>
+            ...who likes to craft solid and scalable frontend products with
+            great user experiences.
+          </p>
+          <div className="hero-btns">
+            <button>Github</button>
+            <button>Linkedin</button>
+          </div>
+        </div>
+        <img src="src/assets/images/foto.svg" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
