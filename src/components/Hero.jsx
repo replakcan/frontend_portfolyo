20e74cefdc84606a5e7e44c351ebaae1129eea72
replakcan <alper.makcan@gmail.com>
2024-10-05 18:07:21 +0300

    Adds btn images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,19 +2,28 @@ const Hero = () => {
   return (
     <section className="hero-section">
       <div className="hero-content">
-      <div className="header-container">alper</div>
+        <div className="header-container">alper</div>
         <div className="hero-left">
-          <h1>I am a Frontend Developer...</h1>
+          <h1>
+            I am a Frontend <br />
+            Developer...
+          </h1>
           <p>
-            ...who likes to craft solid and scalable frontend products with
-            great user experiences.
+            ...who likes to craft solid and scalable frontend <br />
+            products with great user experiences.
           </p>
           <div className="hero-btns">
-            <button>Github</button>
-            <button>Linkedin</button>
+            <button>
+              <img src="src/assets/images/github-mark.svg" />
+              <span>Github</span>
+            </button>
+            <button>
+              <img src="src/assets/images/github-mark.svg" />
+              <span>Linkedin</span>
+            </button>
           </div>
         </div>
-        <img src="src/assets/images/foto.svg" />
+        <img className="hero-img" src="src/assets/images/foto.svg" />
       </div>
     </section>
   );
