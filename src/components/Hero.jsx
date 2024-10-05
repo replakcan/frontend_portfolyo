076c47f9d390cd1fcf30f08efe8a7aed2e57d23e
replakcan <alper.makcan@gmail.com>
2024-10-05 18:23:58 +0300

    Adds lang and theme buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,16 @@ const Hero = () => {
     <section className="hero-section">
       <div className="hero-content">
         <div className="header-container">alper</div>
+        <button className="lang">LANG</button>
+        <button className="dark-theme">DarkTheme</button>
         <div className="hero-left">
           <h1>
             I am a Frontend <br />
             Developer...
           </h1>
           <p>
-            ...who likes to craft solid and scalable frontend <br />
-            products with great user experiences.
+            ...who likes to craft solid and scalable frontend products with
+            great user experiences.
           </p>
           <div className="hero-btns">
             <button>
