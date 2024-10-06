e37246f9c2b6c7a4131244422183bde123e17afa
replakcan <alper.makcan@gmail.com>
2024-10-06 21:20:52 +0300

    Skills css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,38 +1,31 @@
 const Skills = () => {
   return (
     <section className="skills-section">
+      <h1>Skills</h1>
       <div className="skills-container">
         <div className="skill">
-          <img src="src/assets/images/github-mark.svg" />
-          <p>TAZDİN</p>
+          <img src="src/assets/images/skills/js-logo 1.svg" />
+          <p>JAVASCRIPT</p>
         </div>
         <div className="skill">
-          <img src="src/assets/images/github-mark.svg" />
-          <p>TAZDİN</p>
+          <img src="src/assets/images/skills/node-js.svg" />
+          <p>NODE</p>
         </div>
         <div className="skill">
-          <img src="src/assets/images/github-mark.svg" />
-          <p>TAZDİN</p>
+          <img src="src/assets/images/skills/Rectangle 35.svg" />
+          <p>REACT</p>
         </div>
         <div className="skill">
-          <img src="src/assets/images/github-mark.svg" />
-          <p>TAZDİN</p>
+          <img src="src/assets/images/skills/Group 101.svg" />
+          <p>VS CODE</p>
         </div>
         <div className="skill">
-          <img src="src/assets/images/github-mark.svg" />
-          <p>TAZDİN</p>
+          <img src="src/assets/images/skills/Group 101(1).svg" />
+          <p>REDUX</p>
         </div>
         <div className="skill">
-          <img src="src/assets/images/github-mark.svg" />
-          <p>TAZDİN</p>
-        </div>
-        <div className="skill">
-          <img src="src/assets/images/github-mark.svg" />
-          <p>TAZDİN</p>
-        </div>
-        <div className="skill">
-          <img src="src/assets/images/github-mark.svg" />
-          <p>TAZDİN</p>
+          <img src="src/assets/images/skills/figma-logo 1.svg" />
+          <p>FIGMA</p>
         </div>
       </div>
     </section>
