3594a47b856f298e81a4cfca9dba46713cf6b056
replakcan <alper.makcan@gmail.com>
2024-10-06 20:13:21 +0300

    Footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,26 @@
 const Footer = () => {
   return (
     <section className="footer-section">
-      Footer
       <div className="footer-container">
         <h1>Send me a message!</h1>
-        <p>Got a question or proposal, or just want to say hello? Go ahead.</p>
-        <a>replakcan@repla.com</a>
-        <div>
-          <img src="src/assets/images/twitter 1.png" alt="" />
-          <img src="src/assets/images/codepen 1.svg" alt="" />
-          <img src="src/assets/images/at-sign 1.svg" alt="" />
-          <img src="src/assets/images/instagram 1.svg" alt="" />
+        <p>
+          Got a question or proposal, or just want <br /> to say hello? Go
+          ahead.
+        </p>
+        <a href="">replakcan@repla.com</a>
+        <div className="socials">
+          <a href="">
+            <img src="src/assets/images/twitter 1.png" alt="" />
+          </a>
+          <a href="">
+            <img src="src/assets/images/codepen 1.svg" alt="" />
+          </a>
+          <a href="">
+            <img src="src/assets/images/at-sign 1.svg" alt="" />
+          </a>
+          <a href="">
+            <img src="src/assets/images/instagram 1.svg" alt="" />
+          </a>
         </div>
       </div>
     </section>
