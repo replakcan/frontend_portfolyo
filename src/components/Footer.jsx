1d5aa92777d89f77dad3bb82eb5df7f1ccc920ee
replakcan <alper.makcan@gmail.com>
2024-10-06 15:44:51 +0300

    Updates footer section css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,20 @@
 const Footer = () => {
-  return <section className="footer-section">Footer</section>;
+  return (
+    <section className="footer-section">
+      Footer
+      <div className="footer-container">
+        <h1>Send me a message!</h1>
+        <p>Got a question or proposal, or just want to say hello? Go ahead.</p>
+        <a>replakcan@repla.com</a>
+        <div>
+          <img src="src/assets/images/twitter 1.png" alt="" />
+          <img src="src/assets/images/codepen 1.svg" alt="" />
+          <img src="src/assets/images/at-sign 1.svg" alt="" />
+          <img src="src/assets/images/instagram 1.svg" alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
