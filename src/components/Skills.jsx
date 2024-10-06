117f893ef2cd37d4a8b39f55d5f581eb93b4fd6c
replakcan <alper.makcan@gmail.com>
2024-10-06 18:05:36 +0300

    Updates css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,40 @@
 const Skills = () => {
-  return <section className="skills-section">Skills</section>;
+  return (
+    <section className="skills-section">
+      <div className="skills-container">
+      <div>
+        <img src="src/assets/images/github-mark.svg"/>
+        <p>TAZDİN</p>
+      </div>
+      <div>
+        <img src="src/assets/images/github-mark.svg"/>
+        <p>TAZDİN</p>
+      </div>
+      <div>
+        <img src="src/assets/images/github-mark.svg"/>
+        <p>TAZDİN</p>
+      </div>
+      <div>
+        <img src="src/assets/images/github-mark.svg"/>
+        <p>TAZDİN</p>
+      </div>
+      <div>
+        <img src="src/assets/images/github-mark.svg"/>
+        <p>TAZDİN</p>
+      </div>
+      <div>
+        <img src="src/assets/images/github-mark.svg"/>
+        <p>TAZDİN</p>
+      </div>
+      <div>
+        <img src="src/assets/images/github-mark.svg"/>
+        <p>TAZDİN</p>
+      </div>
+      
+      
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
