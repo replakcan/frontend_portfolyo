fd60eb8306b12a231a6ad7fe3a4b439d07b66e25
replakcan <alper.makcan@gmail.com>
2024-10-07 18:15:36 +0300

    Sends axios post request when componentdidmount

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,32 @@ import Hero from "./layouts/Hero";
 import Profile from "./layouts/Profile";
 import Projects from "./layouts/Projects";
 import Skills from "./layouts/Skills";
-
+import axios from "axios";
+import { langData } from "./data";
 
 function App() {
-
   const [darkMode, setDarkMode] = useLocalStorage("DarkMode", false);
   const [lang, setLang] = useLocalStorage("x-lang", "TR");
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    axios
+      .post("https://reqres.in/api/workintech", langData)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [lang]);
 
   return (
-    <main className={darkMode ? 'dark' : ''}>
-      <Hero lang={lang} setLang={setLang} darkMode={darkMode} setDarkMode={setDarkMode} />
+    <main className={darkMode ? "dark" : ""}>
+      <Hero
+        lang={lang}
+        setLang={setLang}
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+      />
       <Skills />
       <Profile />
       <Projects />
