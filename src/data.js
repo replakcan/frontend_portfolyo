af6b7ee9b365a80a72fd9b63b173328121f034a2
replakcan <alper.makcan@gmail.com>
2024-10-07 13:17:58 +0300

    Adds data file

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const languagesData = {
+  en: {
+    hero: {
+      interactive: ["lang", "DarkTheme"],
+      title: "Frontend Developer",
+      para: "...who likes to craft solid and scalablefrontend products with great user experiences.",
+      name: "Alper",
+    },
+    skills: {
+      main_title: "Skills",
+    },
+    profile: {
+      main_title: "Profile",
+      sub_title: ["Basic Information", "About Me"],
+      profile_info: {
+        birth_date: "06.11.1996",
+        city: "Istanbul",
+        education:
+          "Yildiz Technical University, Naval Architecture and Marine Engineering, 2023",
+        position: "Frontend, UI",
+      },
+      about_me:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est mollitia adipisci facilis voluptatem dicta neque eaque quia velit, ea, perferendis inventore et aliquam quibusdam alias eligendi assumenda quis ullam doloribus.",
+    },
+    projects: {
+      main_title: "Projects",
+      project: {
+        title: "Workintech",
+        info: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        vercel_link: "View Site",
+      },
+    },
+    footer: {
+      title: "Send me a message!",
+      para: "Got a question or proposal, or just want to say hello? Go ahead.",
+    },
+  },
+  tr: {
+    hero: {
+      interactive: ["dil", "GeceModu"],
+      title: "Ön uç geliştirici",
+      para: "...harika kullanıcı deneyimleriyle sağlam ve ölçeklenebilir ön uç ürünleroluşturmayı seven.",
+      name: "Alper",
+    },
+    skills: {
+      main_title: "Beceriler",
+    },
+    profile: {
+      main_title: "Profil",
+      sub_title: ["Genel Bilgiler", "Hakkımda"],
+      profile_info: {
+        birth_date: "06.11.1996",
+        city: "İstanbul",
+        education:
+          "Yıldız Teknik Üniversitesi, Gemi İnş. ve Gemi Mak. Müh., 2023",
+        position: "Frontend, UI",
+      },
+      about_me:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est mollitia adipisci facilis voluptatem dicta neque eaque quia velit, ea, perferendis inventore et aliquam quibusdam alias eligendi assumenda quis ullam doloribus.",
+    },
+    projects: {
+      main_title: "Projeler",
+      project: {
+        title: "Workintech",
+        info: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        vercel_link: "Görüntüle",
+      },
+    },
+    footer: {
+      title: "Bana mesaj yolla!",
+      para: "Danışmak istediğiniz veya söylemek istediğiniz herhangi bir şeyiniz varsa",
+    },
+  },
+};
