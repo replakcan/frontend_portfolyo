3278637694f43de150ceb8cd15e43d0e341d762c
replakcan <alper.makcan@gmail.com>
2024-10-07 22:47:39 +0300

    Updates email data

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="footer-container">
         <h1>{data.footer.title}</h1>
         <p>{data.footer.para}</p>
-        <a href="">replakcan@repla.com</a>
+        <a href="">{data.footer.email}</a>
         <div className="socials">
           <a href="">
             <img src="./images/footer/twitter 1.png" alt="twitter icon" />
