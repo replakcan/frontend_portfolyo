0645ad31b4e1248fd520c636b11f1860ee7b0298
replakcan <alper.makcan@gmail.com>
2024-10-07 23:35:29 +0300

    Updates profile data

--- src/layouts/Profile.jsx
+++ src/layouts/Profile.jsx
@@ -1,47 +1,40 @@
-import { useState } from "react";
-
-const initialValues = {
-  birthDate: "24.03.1996",
-  province: "Ankara",
-  education: "Hacettepe Ünv. Biyoloji Lisans, 2016",
-  position: "Frontend, UI",
-};
+import { useContext } from "react";
+import { LangContext } from "../contexts/LangContext";
 
 const Profile = () => {
-  const [profileData, setProfileData] = useState(initialValues);
+  const { data, lang } = useContext(LangContext);
 
   return (
     <section className=" profile-section dark:bg-gray-500">
-      <h1 className="text-limon dark:text-gray-800 font-bold">Profile</h1>
+      <h1 className="text-limon dark:text-gray-800 font-bold">
+        {data.profile.main_title}
+      </h1>
       <div className="profile-container">
         <div className="info-container">
-          <h2 className="info-heading text-3xl">Basic Information</h2>
+          <h2 className="info-heading text-3xl">{data.profile.sub_title[0]}</h2>
           <p className="info">
-            <span className="text-limon">Doğum tarihi </span>
-            {profileData.birthDate}
+            <span className="text-limon">{lang === "TR" ? "Doğum tarihi" : "Date of Birth"} </span>
+            {data.profile.profile_info.birth_date}
           </p>
           <p className="info">
-            <span className="text-limon">İkamet Şehri </span>
-            {profileData.province}
+            <span className="text-limon">{lang === "TR" ? "Şehir" : "City"} </span>
+            {data.profile.profile_info.city}
           </p>
           <p className="info">
-            <span className="text-limon">Eğitim Durumu </span>
-            {profileData.education}
+            <span className="text-limon">{lang === "TR" ? "Eğitim" : "Education"} </span>
+            {data.profile.profile_info.education}
           </p>
           <p className="info">
-            <span className="text-limon">Tercih Ettiği Rol </span>
-            {profileData.position}
+            <span className="text-limon">{lang === "TR" ? "Pozisyon" : "Position"} </span>
+            {data.profile.profile_info.position}
           </p>
         </div>
         <img src="./images/hero/foto.svg" />
         <div className="info-content">
-          <h2 className="aboutMe-heading text-3xl">About Me</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
-            mollitia adipisci facilis voluptatem dicta neque eaque quia velit,
-            ea, perferendis inventore et aliquam quibusdam alias eligendi
-            assumenda quis ullam doloribus.
-          </p>
+          <h2 className="aboutMe-heading text-3xl">
+            {data.profile.sub_title[1]}
+          </h2>
+          <p>{data.profile.about_me}</p>
         </div>
       </div>
     </section>
