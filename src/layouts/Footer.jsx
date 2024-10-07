316bcbddcc7d728b2ce2f1750aba91456646ff4f
replakcan <alper.makcan@gmail.com>
2024-10-07 22:38:49 +0300

    Update footer data
    gets the data from axios response

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,25 +1,27 @@
+import { useContext } from "react";
+import { LangContext } from "../contexts/LangContext";
+
 const Footer = () => {
+  const { data } = useContext(LangContext);
+
   return (
     <section className="footer-section dark:bg-gray-300">
       <div className="footer-container">
-        <h1>Send me a message!</h1>
-        <p>
-          Got a question or proposal, or just want <br /> to say hello? Go
-          ahead.
-        </p>
+        <h1>{data.footer.title}</h1>
+        <p>{data.footer.para}</p>
         <a href="">replakcan@repla.com</a>
         <div className="socials">
           <a href="">
-            <img src="./images/footer/twitter 1.png" alt="" />
+            <img src="./images/footer/twitter 1.png" alt="twitter icon" />
           </a>
           <a href="">
-            <img src="./images/footer/codepen 1.svg" alt="" />
+            <img src="./images/footer/codepen 1.svg" alt="codepen icon" />
           </a>
           <a href="">
-            <img src="./images/footer/at-sign 1.svg" alt="" />
+            <img src="./images/footer/at-sign 1.svg" alt="e-mail icon" />
           </a>
           <a href="">
-            <img src="./images/footer/instagram 1.svg" alt="" />
+            <img src="./images/footer/instagram 1.svg" alt="instagram icon" />
           </a>
         </div>
       </div>
