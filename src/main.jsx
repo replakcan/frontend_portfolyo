88e4d2fc7023c48ba6b9a3583979dff8ac576312
replakcan <alper.makcan@gmail.com>
2024-10-09 00:57:45 +0300

    Deletes unneccessary import

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -6,11 +5,9 @@ import { LangContextProvider } from "./contexts/LangContext.jsx";
 import { ThemeContextProvider } from "./contexts/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <LangContextProvider>
-      <ThemeContextProvider>
-        <App />
-      </ThemeContextProvider>
-    </LangContextProvider>
-  </StrictMode>
+  <LangContextProvider>
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
+  </LangContextProvider>
 );
