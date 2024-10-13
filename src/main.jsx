89916a2e1a96c4c4b02c39da98b14a80b4c66dae
replakcan <alper.makcan@gmail.com>
2024-10-13 15:46:50 +0300

    Adds tanStackDevTools container

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { LangContextProvider } from "./contexts/LangContext.jsx";
 import { ThemeContextProvider } from "./contexts/ThemeContext.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -14,5 +15,6 @@ createRoot(document.getElementById("root")).render(
         <App />
       </ThemeContextProvider>
     </LangContextProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
