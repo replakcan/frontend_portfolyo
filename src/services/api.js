dd9a96d9efc12bc6c8c5fd785b207ec6889702cb
replakcan <alper.makcan@gmail.com>
2024-10-13 15:04:36 +0300

    Comment outs unused tanStack queries and apis

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-import { langData, projects } from "../data";
+import { langData, /* projects */ } from "../data";
 
-export const getProjectData = async () => {
+/* export const getProjectData = async () => {
   console.log("projects: ", projects);
   
   return await axios
     .post("https://reqres.in/api/workintech", projects)
     .then((res) => res.data)
     .catch((err) => console.log(err.message));
-};
+}; */
 
 export const getMainData = async () => {
   console.log("main: ", langData);
--- src/services/tanStack.js
+++ src/services/tanStack.js
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { getMainData, getProjectData } from "./api";
+import { getMainData, /* getProjectData */ } from "./api";
 
-export function useProjectsData() {
+/* export function useProjectsData() {
   return useQuery({
     queryKey: ["projectData"],
     queryFn: getProjectData,
   });
-}
+} */
 
 export function useMainData() {
   return useQuery({
