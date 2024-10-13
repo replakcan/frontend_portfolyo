db2489e73086d674b3e497daee2ecd67ff59c552
replakcan <alper.makcan@gmail.com>
2024-10-13 11:15:11 +0300

    Updates getProjectsData api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,11 +3,8 @@ import { projects } from "../data";
 
 export const getProjectData = async () => {
   console.log("projects: ", projects);
-  try {
-    const res = await axios.post("https://reqres.in/api/workintech", projects);
-    return res.data; // This returns the data after the promise is resolved
-  } catch (error) {
-    console.error("Error fetching project data: ", error);
-    return undefined; // Return undefined or handle the error as needed
-  }
+  return await axios
+    .post("https://reqres.in/api/workintech", projects)
+    .then((res) => res.data)
+    .catch((err) => console.log(err.message));
 };
