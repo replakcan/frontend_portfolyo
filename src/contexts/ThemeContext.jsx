853f7dc16da8f4996259830245729636cc394584
replakcan <alper.makcan@gmail.com>
2024-10-13 16:53:14 +0300

    Adds user prefs for darkmode initial value

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -3,9 +3,17 @@ import useLocalStorage from "../hooks/useLocalStorage";
 
 export const ThemeContext = createContext();
 
+const userDarkModePref =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+console.log("user dark mode: ", userDarkModePref);
+
 // eslint-disable-next-line react/prop-types
 export const ThemeContextProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useLocalStorage("DarkMode", false);
+  const [darkMode, setDarkMode] = useLocalStorage(
+    "DarkMode",
+    userDarkModePref ? true : false
+  );
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
